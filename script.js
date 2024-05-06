4732a71e758a8b905fda4e311dd49db5ca5d5e16
ghm7 <pozzisgnosis@outlook.com>
2024-05-05 23:35:57 -0300

    Form, placeholders, toast, modal

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+const toast = document.querySelector('#liveToast');
 const progressBar = document.querySelector('#progress-bar');
+
 const documentHeight = document.documentElement.scrollHeight;
 const clientHeight = document.documentElement.clientHeight;
-
-console.log(progressBar);
 
 window.addEventListener('scroll', () => {
   const scroll = this.scrollY;
   const percentage = (scroll / (documentHeight - clientHeight)) * 100;
 
   progressBar.style.width = percentage + '%';
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toastAlert = bootstrap.Toast.getOrCreateInstance(toast);
+  toastAlert.show();
 });
